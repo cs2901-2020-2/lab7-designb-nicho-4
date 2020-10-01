0eb426013e7163b05cc54a2c67b11d3d9afe7c31
Alejandro  Otero <alejandrotero@Alejandros-MacBook-Air.local>
2020-10-01 09:53:21 -0500

    Recreate the game

--- src/main/java/buscaminas.java
+++ src/main/java/buscaminas.java
@@ -6,32 +6,39 @@ public class buscaminas {
 
     static final Logger logger = Logger.getLogger(buscaminas.class.getName());
 
-    public static void creartablas(int[][] tabla, String[][] tablajuego , int n){
+    public int [][] tabla;
+    public int tamano;
+    public int coordx, coordy;
+
+    public buscaminas(int n){
+        tabla = new int[n][n];
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
                 tabla[i][j] = 0;
-                tablajuego[i][j] = "o";
             }
         }
-        set_minas(tabla, tablajuego, n);
+        set_minas(tabla, n);
     }
 
-    public static void set_minas(int[][] tabla, String [][] tablajuego, int n){
+    public static void set_minas(int[][] tabla, int n){
         Random rand = new Random();
         int cant_minas = rand.nextInt(n+1);
         int rand_x = rand.nextInt(n);
         int rand_y = rand.nextInt(n);
         for (int i = 0; i < cant_minas; i++){
-            if (tabla[rand_x][rand_y] == 0)
+            if (tabla[rand_x][rand_y] == 0) {
                 tabla[rand_x][rand_y] = 1;
-            else
+            }
+            else {
                 rand_x = rand.nextInt(n);
                 rand_y = rand.nextInt(n);
+                i--;
+            }
         }
-        imprimirtabla(tabla, tablajuego, n);
-        escoger_casilla(tabla, tablajuego, n);
+        escoger_casilla(tabla, n);
     }
 
+    /*
     public static void imprimirtabla(int [][] tabla,String[][] tablajuego, int n){
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
@@ -40,8 +47,9 @@ public class buscaminas {
             logger.info("\n");
         }
     }
+     */
 
-    public static void escoger_casilla(int[][] tabla, String[][] tablajuego, int n){
+    public static void escoger_casilla(int[][] tabla, int n, int coordx, int coordy){
         logger.info("Ingresa coordenadas en x: ");
         Scanner value_x= new Scanner(System.in);
         logger.info("Ingresa coordenadas en y: ");
@@ -51,33 +59,24 @@ public class buscaminas {
 
         while (x >= n || y >= n){
             logger.info("Ingresa coordenadas validas: ");
-            Scanner value_xn= new Scanner(System.in);
-            Scanner value_yn= new Scanner(System.in);
-            x = value_xn.nextInt();
-            y = value_yn.nextInt();
+            Scanner val_xn= new Scanner(System.in);
+            Scanner val_yn= new Scanner(System.in);
+            x = val_xn.nextInt();
+            y = val_yn.nextInt();
         }
+
         if (tabla[x][y] == 0){
-            tablajuego[x][y] = "x";
-            imprimirtabla(tabla, tablajuego,n);
-            escoger_casilla(tabla, tablajuego, n);
+            //tablajuego[x][y] = "x";
+            //imprimirtabla(tabla, tablajuego,n);
+            escoger_casilla(tabla, n);
         } else {
             logger.info("Perdiste");
-            imprimirtabla(tabla, tablajuego, n);
+            //imprimirtabla(tabla, tablajuego, n);
         }
     }
 
+    public void play(){
 
-    public static void main(String[] args){
-        logger.info("Ingresa el tamaño de la tabla: ");
-        Scanner scannerObj = new Scanner(System.in);
-
-        int n = scannerObj.nextInt();
-
-        int[][] tabla = new int[n][n];
-        String[][] tablajuego = new String[n][n];
-        creartablas(tabla, tablajuego, n);
-
-        logger.info("Fin del juego...");
     }
 
 }
